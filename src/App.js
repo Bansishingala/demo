0ce0481fc1f7e9f060917b0a344e4eb9702c9743
bansishingala <shingalabansi8@gmail.com>
2022-06-01 15:53:26 +0530

    1-6-2022

--- src/App.js
+++ src/App.js
@@ -6,131 +6,7 @@ import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 function App() {
 
-  //1// const person = {
-  //   name: "Amit",
-  //   age: 25
-  // }
-  //  for(let k in person){
-  //   console.log(person[k]);
-  //  }
-
-
-  //2// const person = [
-  //   {
-  //     name: "Amit",
-  //     age: 25,
-  //   },
-  //   {
-  //     name: "Piyush",
-  //     age: 40,
-  //   },
-  // ];
-
-  // person.map((p)=>console.log(p.name,p.age));
-
-
-
-  //3// const person = {
-  //   name: "Amit",
-  //   age: 25,
-  //   course: [
-  //     "C",
-  //     "HTML"
-  //   ]
-  // }
-
-  // console.log(person.name,person.age);
-  // person.course.map((p)=>console.log(p));
-
-
-  //4 //  const person = [
-  //   {
-  //     name: "Amit",
-  //     age: 25,
-  //     course: [
-  //       "C",
-  //       "HTML"
-  //     ]
-  //   },
-  //   {
-  //     name: "Ajay",
-  //     age: 40,
-  //     course: [
-  //       "Java",
-  //       "JavaScript"
-  //     ]
-  //   }
-  // ]
-
-  // person.map((p)=>{
-  //   console.log(p.name,p.age);
-  //   p.course.map((p1)=> console.log(p1))
-  // })
-
-
-
-  //5 // const myObj = {
-  //   name: "John",
-  //   age: 30,
-  //   cars: {
-  //     car1: "Ford",
-  //     car2: "BMW",
-  //     car3: "Fiat"
-  //   }
-  // }
-
-  // for (let k in myObj) {
-  //   if (k === 'cars') {
-  //     for (let j in myObj[k]) {
-  //       console.log(myObj[k][j]);
-  //     }
-  //   }else{
-  //     console.log(myObj[k]);
-  //   }
-  // }
-
-
-  //6  // const data = {
-  //   personal_info: {
-  //     name: 'amit',
-  //     age: 25,
-  //     city: 'surat'
-  //   },
-  //   courses: ["C", "JavaScript", "React"],
-  //   branches: {
-  //     rwl: {
-  //       admission: 50,
-  //       vacant_seat: 10
-  //     },
-  //     rw2: {
-  //       admission: 30,
-  //       vacant_seat: 20
-  //     },
-  //     rw3: {
-  //       admission: 25,
-  //       vacant_seat: 25
-  //     },
-  //     rw4: {
-  //       admission: 40,
-  //       vacant_seat: 10
-  //     }
-  //   }
-  // }
-
-  // for (let k in data) {
-  //   if (k === 'personal_info') {
-  //       console.log(data[k].name,data[k].age,data[k].city);
-  //   }else if(k === 'courses'){
-  //       data[k].map((p)=> console.log(p))
-  //   }else{
-  //     for(let j in data[k]){
-  //       console.log(data[k][j].admission,data[k][j].vacant_seat);
-  //     }
-  //   }
-  // }
-
-
-  data: [
+  let data = [
     {
       id: 101,
       name: 'Abacavir',
@@ -173,34 +49,111 @@ function App() {
     }
   ];
 
+  let data1=
+[
+    {
+      name: "amit",
+      age: 35,
+      salary: 40000,
+      bonus: 1000,
+      status: true
+    },
+    {
+      name: "ajay",
+      age: 25,
+      salary: 38000,
+      bonus: 2000,
+      status: false
+    },
+    {
+      name: "mayur",
+      age: 23,
+      salary: 50000,
+      bonus: 500,
+      status: true
+    },
+    {
+      name: "jay",
+      age: 29,
+      salary: 35000,
+      bonus: 600,
+      status: true
+    },
+    {
+      name: "raj",
+      age: 33,
+      salary: 22000,
+      bonus: 2000,
+      status: true
+    },
+  ]
 
+  let fdata = data.filter ((d) =>d.status === true && d.expiry >= 2022 )
+  console.log(fdata);
+  let fdata1 = data1.filter ((d1) =>d1.age >= 21 && d1.status === true )
+  console.log(fdata1);
 
-  return(
+  let ans=  fdata.reduce((acc,d,i)=>acc + d.price ,0)
+  console.log(ans);
+  let ans1 = fdata1.reduce ((acc,d1,i)=> acc + d1.salary ,0)
+  console.log(ans1); 
+
+  return (
     <div>
-    <table>
-      <tr>
-        <td>id</td>
-        <td>name</td>
-        <td>quantity</td>
-        <td>price</td>
-        <td>expiry</td>
-        <td>status</td>
-      </tr>
-      {
-        Data.map((d) => {
-          return(
-          <tr>
-            <th>{d.id}</th>
-            <th>{d.name}</th>
-            <th>{d.quantity}</th>
-            <th>{d.price}</th>
-            <th>{d.expiry}</th>
-            <th>{d.status}</th>
-          </tr>
-      )
-    })
-  }
-    </table>
+      <table border="1">
+        <tr>
+          <td>Id</td>
+          <td>Name</td>
+          <td>Quantity</td>
+          <td>Price</td>
+          <td>Expiry</td>
+          <td>Status</td>
+        </tr>
+        {
+          data.map((d) => {
+            let {id,name,quantity,price,expiry,status} = d 
+            return (
+              <tr>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{quantity}</td>
+                <td>{price}</td>
+                <td>{expiry}</td>
+                <td>{status.toString()}</td>
+              </tr>
+            )
+          })
+        }
+      </table>
+      <table border="1">
+        <tr>
+          <td>Name</td>
+          <td>age</td>
+          <td>salary</td>
+          <td>bonus</td>
+          <td>Status</td> 
+        </tr>
+        {
+          data1.map((d1) => {
+            let {name,age,salary,bonus,status} = d1 
+            return (
+              <tr>
+                <td>{name}</td>
+                <td>{age}</td>
+                <td>{salary}</td>
+                <td>{bonus}</td>
+                <td>{status.toString()}</td>
+              </tr>
+            )
+          })
+        }
+      </table>
     </div>
+  );
+
   
-  )
+
+}
+
+
+export default App;
