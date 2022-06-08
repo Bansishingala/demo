a64bb51fee8287f87792727399a600823db8818a
bansishingala <shingalabansi8@gmail.com>
2022-06-08 09:57:57 +0530

    time

--- src/Container/Time/Time.js
+++ src/Container/Time/Time.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Time extends Component {
+class Time extends Component { 
         constructor(props) {
             super(props);
             this.state = {
